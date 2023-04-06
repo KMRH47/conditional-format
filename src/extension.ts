90e3923ca56ba50ede0522bf69ebe66a3de26957
Karsten <kramze47@gmail.com>
2023-04-06 14:07:44 +0200

    Extract messages to own file

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,52 @@
 import * as vscode from "vscode";
+import {
+  UNABLE_TO_FORMAT,
+  DOCUMENT_ALREADY_FORMATTED,
+  SELECTION_ALREADY_FORMATTED,
+} from "./strings";
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand("conditionalFormat.format", async () => {
-    const editor = vscode.window.activeTextEditor;
+  const disposable = vscode.commands.registerCommand(
+    "conditionalFormat.format",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
 
-    if (!editor) {
-      return;
+      if (!editor) {
+        return;
+      }
+
+      const document = editor.document;
+      const selection = editor.selection;
+      const fullRange = new vscode.Range(0, 0, document.lineCount, 0);
+      const formatOptions = getFormatOptions(document);
+      const formattedTextEdits = await getFormattedTextEdits(
+        document,
+        fullRange,
+        formatOptions
+      );
+
+      if (formattedTextEdits === undefined) {
+        showError(UNABLE_TO_FORMAT);
+        return;
+      }
+
+      if (selection.isEmpty) {
+        formatDocument(editor, formattedTextEdits);
+      } else {
+        formatSelection(editor, selection, formattedTextEdits);
+      }
     }
-
-    const document = editor.document;
-    const selection = editor.selection;
-    const fullRange = new vscode.Range(0, 0, document.lineCount, 0);
-    const formatOptions = getFormatOptions(document);
-    const formattedTextEdits = await getFormattedTextEdits(document, fullRange, formatOptions);
-
-    if (formattedTextEdits === undefined) {
-      showError("Unable to execute the formatting command.");
-      return;
-    }
-
-    if (selection.isEmpty) {
-      formatDocument(editor, formattedTextEdits);
-    } else {
-      formatSelection(editor, selection, formattedTextEdits);
-    }
-  });
+  );
 
   context.subscriptions.push(disposable);
 }
 
-function getFormatOptions(document: vscode.TextDocument): vscode.FormattingOptions | undefined {
-  return vscode.workspace.getConfiguration("editor", document.uri).get<vscode.FormattingOptions>("formatOnSave");
+function getFormatOptions(
+  document: vscode.TextDocument
+): vscode.FormattingOptions | undefined {
+  return vscode.workspace
+    .getConfiguration("editor", document.uri)
+    .get<vscode.FormattingOptions>("formatOnSave");
 }
 
 async function getFormattedTextEdits(
@@ -46,9 +62,12 @@ async function getFormattedTextEdits(
   );
 }
 
-async function formatDocument(editor: vscode.TextEditor, formattedTextEdits: vscode.TextEdit[]): Promise<void> {
+async function formatDocument(
+  editor: vscode.TextEditor,
+  formattedTextEdits: vscode.TextEdit[]
+): Promise<void> {
   if (formattedTextEdits.length === 0) {
-    showInfo("The entire document is already formatted.");
+    showInfo(DOCUMENT_ALREADY_FORMATTED);
   } else {
     await vscode.commands.executeCommand("editor.action.formatDocument");
   }
@@ -74,7 +93,7 @@ async function formatSelection(
   );
 
   if (!anyEditApplied) {
-    showInfo("The selected code block is already formatted.");
+    showInfo(SELECTION_ALREADY_FORMATTED);
   }
 }
 
---
+++ src/strings.ts
@@ -0,0 +1,3 @@
+export const UNABLE_TO_FORMAT = "Unable to execute the formatting command.";
+export const DOCUMENT_ALREADY_FORMATTED = "The entire document is already formatted.";
+export const SELECTION_ALREADY_FORMATTED = "The selected code block is already formatted.";
