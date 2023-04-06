d7530a09a66f3202d13b14b8136d9afaab5d300b
Karsten <kramze47@gmail.com>
2023-04-06 14:07:04 +0200

    Update formatting logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,89 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('conditionalFormat.format', async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
+  const disposable = vscode.commands.registerCommand("conditionalFormat.format", async () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      return;
+    }
+
+    const document = editor.document;
+    const selection = editor.selection;
+    const fullRange = new vscode.Range(0, 0, document.lineCount, 0);
+    const formatOptions = getFormatOptions(document);
+    const formattedTextEdits = await getFormattedTextEdits(document, fullRange, formatOptions);
+
+    if (formattedTextEdits === undefined) {
+      showError("Unable to execute the formatting command.");
+      return;
+    }
+
+    if (selection.isEmpty) {
+      formatDocument(editor, formattedTextEdits);
+    } else {
+      formatSelection(editor, selection, formattedTextEdits);
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+function getFormatOptions(document: vscode.TextDocument): vscode.FormattingOptions | undefined {
+  return vscode.workspace.getConfiguration("editor", document.uri).get<vscode.FormattingOptions>("formatOnSave");
+}
+
+async function getFormattedTextEdits(
+  document: vscode.TextDocument,
+  range: vscode.Range,
+  formatOptions: vscode.FormattingOptions | undefined
+): Promise<vscode.TextEdit[] | undefined> {
+  return await vscode.commands.executeCommand<vscode.TextEdit[]>(
+    "vscode.executeFormatRangeProvider",
+    document.uri,
+    range,
+    formatOptions
+  );
+}
+
+async function formatDocument(editor: vscode.TextEditor, formattedTextEdits: vscode.TextEdit[]): Promise<void> {
+  if (formattedTextEdits.length === 0) {
+    showInfo("The entire document is already formatted.");
+  } else {
+    await vscode.commands.executeCommand("editor.action.formatDocument");
+  }
+}
+
+async function formatSelection(
+  editor: vscode.TextEditor,
+  selection: vscode.Selection,
+  formattedTextEdits: vscode.TextEdit[]
+): Promise<void> {
+  let anyEditApplied = false;
+
+  await editor.edit(
+    (editBuilder) => {
+      for (const textEdit of formattedTextEdits) {
+        if (selection.contains(textEdit.range)) {
+          editBuilder.replace(textEdit.range, textEdit.newText);
+          anyEditApplied = true;
         }
+      }
+    },
+    { undoStopBefore: false, undoStopAfter: false }
+  );
 
-        const document = editor.document;
-        const selection = editor.selection;
+  if (!anyEditApplied) {
+    showInfo("The selected code block is already formatted.");
+  }
+}
 
-        const range = selection.isEmpty
-            ? new vscode.Range(document.positionAt(0), document.positionAt(document.getText().length))
-            : new vscode.Range(selection.start, selection.end);
+function showInfo(message: string): void {
+  vscode.window.showInformationMessage(message);
+}
 
-        const formatOptions = vscode.workspace.getConfiguration('editor', document.uri).get<vscode.FormattingOptions>('formatOnSave');
-        const formattedTextEdits = await vscode.commands.executeCommand<vscode.TextEdit[]>('vscode.executeFormatRangeProvider', document.uri, range, formatOptions);
-
-        if (formattedTextEdits === undefined) {
-            vscode.window.showErrorMessage('Unable to execute the formatting command.');
-            return;
-        }
-        
-        if (formattedTextEdits.length === 0) {
-            const message = selection.isEmpty
-                ? 'The entire document is already formatted.'
-                : 'The selected code block is already formatted.';
-            vscode.window.showInformationMessage(message);
-            return;
-        }
-
-        await editor.edit(editBuilder => {
-            for (const textEdit of formattedTextEdits) {
-                editBuilder.replace(textEdit.range, textEdit.newText);
-            }
-        });
-    });
-
-    context.subscriptions.push(disposable);
+function showError(message: string): void {
+  vscode.window.showErrorMessage(message);
 }
 
 export function deactivate() {}
