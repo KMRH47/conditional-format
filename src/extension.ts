049bdb1fd8023df0210272bacfffcf18878645a2
Karsten <kramze47@gmail.com>
2023-07-23 12:21:20 +0200

    Reformat

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,10 @@ import * as vscode from "vscode";
 import { formatCommand } from "./commands/formatCommand";
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand("conditionalFormat.format", formatCommand);
+  const disposable = vscode.commands.registerCommand(
+    "conditionalFormat.format",
+    formatCommand
+  );
   context.subscriptions.push(disposable);
 }
 
